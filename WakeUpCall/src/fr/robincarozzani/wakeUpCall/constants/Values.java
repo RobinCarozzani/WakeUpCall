e6c28c087a446a3d0e59d5956557e599a023be5c
Robin Carozzani <contact@robincarozzani.fr>
2015-02-08 05:30:57 +0100

    prepare migration to android studio

--- WakeUpCall/src/fr/robincarozzani/wakeUpCall/constants/Values.java
+++
@@ -1,9 +0,0 @@
-package fr.robincarozzani.wakeUpCall.constants;
-
-public class Values {
-
-	public static final int TRUE = 1;
-	public static final int FALSE = 0;
-	
-	public static final int IGNORE = -1; 
-}
--- WakeUpCall/src/fr/robincarozzani/wakeUpCall/constants/db/AlarmDatesDB.java
+++
@@ -1,9 +0,0 @@
-package fr.robincarozzani.wakeUpCall.constants.db;
-
-public class AlarmDatesDB {
-	
-	public static final String TABLENAME = "AlarmDates";
-	
-	public static final String ALARMID = "idAlarm";
-	public static final String ACTIVATIONDATE = "activationDate";
-}
